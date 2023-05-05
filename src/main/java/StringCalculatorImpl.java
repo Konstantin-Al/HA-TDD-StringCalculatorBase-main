7ba435d2c3b804e7f64d0b116081618f75270f94
Konstantin-Al <76794873+Konstantin-Al@users.noreply.github.com>
2023-05-05 14:06:25 +0300

    Initial commit  1 DONE

--- src/main/java/StringCalculatorImpl.java
+++ src/main/java/StringCalculatorImpl.java
@@ -1,8 +1,33 @@
+import java.util.Arrays;
+
 public class StringCalculatorImpl implements StringCalculator {
+
+
+    private int get_int_from_str(String num_str) {
+        try{
+            int number = Integer.parseInt(num_str.trim());
+            return number;
+        }
+        catch (NumberFormatException ex){
+            ex.printStackTrace();
+            return 0;
+        }
+    }
 
     @Override
     public int add(String input) {
-        //TODO implement
-        return -1;
+
+        if (input.length() == 0) {
+            return 0;
+        }
+        else {
+            String[] splitStr = input.split(",");
+            if (splitStr.length == 1) {
+                return get_int_from_str(splitStr[0]);
+            }
+            else {
+                return get_int_from_str(splitStr[0]) + get_int_from_str(splitStr[1]);
+            }
+        }
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -15,4 +15,25 @@ public class StringCalculatorTest {
     public void testEmptyStringReturnsZero() {
         Assertions.assertEquals(0, calculator.add(""));
     }
+
+    @Test
+    public void testNotEmptyStringReturns5() {
+        Assertions.assertEquals(5, calculator.add("5"));
+    }
+
+    @Test
+    public void testNotEmptyStringReturns0() {
+        Assertions.assertEquals(0, calculator.add("0"));
+    }
+
+    @Test
+    public void testNotEmptyStringReturnsSum5() {
+        Assertions.assertEquals(5, calculator.add("2,3"));
+    }
+
+    @Test
+    public void testNotEmptyStringReturnsSum5inputSpace() {
+        Assertions.assertEquals(5, calculator.add("2, 3"));
+    }
+
 }
